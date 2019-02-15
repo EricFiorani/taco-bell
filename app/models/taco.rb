cfe29bca4d2fa966507c9e906baf76700c388c7e
github username <github email address>
2019-02-15 10:40:45 -0500

    model associations connected, some functionality built out for taco model

--- app/models/taco.rb
+++ app/models/taco.rb
@@ -1,2 +1,28 @@
 class Taco < ApplicationRecord
+
+  has_many :ratings
+  has_many :users, through: :ratings
+
+  def full_name
+  end
+
+  def average_rating
+  end
+
+  def most_reviewed
+    self.average_rating + self.ratings.count
+  end
+
+  def most_popular?
+    if self.average_rating >= 3
+      "#{self.full_name} " + "#{self.most_reviewed}"
+    end
+  end
+
+  def self.popular
+    Taco.all.each do |taco|
+      taco.most_popular?
+    end
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+
+  has_many :ratings
+  has_many :tacos, through: :ratings
 end
