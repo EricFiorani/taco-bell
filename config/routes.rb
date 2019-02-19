b01ceda24bd44aa0913a05c3e29e850ae5f620c4
github username <github email address>
2019-02-19 07:54:03 -0500

    added functionality to application/sessions controller, and added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#home'
+
+    get '/signin' => 'sessions#new'
+    post '/signin' => 'sessions#create'
+    post '/signout' => 'sessions#destroy'
+    delete "/signout", to: "sessions#destroy"
+
+    get '/auth/github/callback' => 'sessions#create'
+
+    # post '/filter' => "tacos#filter"
+
 end
